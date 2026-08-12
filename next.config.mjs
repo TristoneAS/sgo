21df91b0d9a87d63caa405b7342ed282b2bc6e7b
Saenz <alejandro.saenz@tristone.com>
2026-08-12 15:34:51 -0600

    Se suben config y package

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  // Permite abrir el app en red interna por hostname (ej. http://tftdelsrv011:3035)
+  allowedDevOrigins: ["tftdelsrv011", "localhost", "127.0.0.1"],
 };
 
 export default nextConfig;
